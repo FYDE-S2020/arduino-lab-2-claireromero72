31dcd08e2361c6259822e0b8336b0382960bf42f
claireromero72 <60532166+claireromero72@users.noreply.github.com>
2020-03-06 14:39:11 -0600

    Finished Program

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -1,3 +1,5 @@
+#include <Blynk.h>
+
 /*************************************************************
   Download latest Blynk library here:
     https://github.com/blynkkk/blynk-library/releases/latest
@@ -31,32 +33,92 @@
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
+#define LED 2
 
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+BlynkTimer timer;
+int time_count = 0; // timer counter global variable
+String content = "";  // null string constant ( an empty string )
+
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token here";
+char auth[] = "SDLaFP95h5Js0d7xaU0CvSprf_nVQz_R";
+const int freq = 5000;     // 5KHz frequency is more than enough. Remember we used 100 before.
+const int ledChannel = 0;  // You can have up to 16 pwm channels (0 to 15)
+const int resolution = 10; // 10 bit resolution for 1023. Freq should be greater than resolution.
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "EE-IOT-Platform-02";
-char pass[32] = "g!TyA>hR2JTy";
+char ssid[32] = "Pittt";//EE-IOT-Platform-02
+char pass[32] = "batgang1";//g!TyA>hR2JTy
+
+
+void myTimerEvent() // Every 10 ms
+{
+    if (time_count % 25 == 0) { // every 250 ms
+        // Do thing that needs to happen every 0.25 seconds
+    }
+    if (time_count == 100){
+             Blynk.virtualWrite(V2, millis() / 1000); // Write the arduino uptime every second
+        time_count = 0; // reset time counter
+    }
+    else {
+        // Send serial data to Blynk terminal
+        char character;
+        while(Serial.available()) { // Check if serial is available every 10 ms
+            character = Serial.read();
+            content.concat(character);
+        }
+        if (content != "") {
+            Blynk.virtualWrite(V3, content);
+            content = ""; // Clear String
+        }  
+    }
+    time_count += 1; // Increment on every tick
+}
+
+
 
 void setup()
 {
   // Serial Monitor
   Serial.begin(115200);
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(LED, ledChannel);
   Blynk.begin(auth, ssid, pass);
+  timer.setInterval(10L, myTimerEvent);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run(); // Initiates BlynkTimer
 }
 
+
+// This function executes whenever V1 is written to.
+// You don't need to call it. Blynk handles the trigger for you.
+BLYNK_WRITE(V0)
+{
+    int pinValue = param.asInt(); 
+
+    if (pinValue == 0) {
+            ledcWrite(ledChannel, 0);// turn LED off
+    }
+    else {
+            ledcWrite(ledChannel, 1023);// turn LED on
+    }
+}
+
+BLYNK_WRITE(1)
+{
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    int val = param.asInt(); // assigning incoming value from pin V1 to a variable
+    ledcWrite(ledChannel, val); // Note that this takes ledChannel as an argument, NOT the pin! Set duty = val.
+}
